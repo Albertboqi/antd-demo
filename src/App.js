0be2da89d72d9cd7e89e4c2505049ccaf2319c80
wangbq <albertboqi@outlook.com>
2020-12-23 23:48:07 +0800

    增加布局

--- src/App.js
+++ src/App.js
@@ -1,49 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
-import { render } from 'react-dom';
-import { ConfigProvider, DatePicker, message, Button, Modal, Input} from 'antd';
 // 由于 antd 组件的默认文案是英文，所以需要修改为中文
 import zhCN from 'antd/es/locale/zh_CN';
-import moment from 'moment';
-import 'moment/locale/zh-cn';
+
 import 'antd/dist/antd.css';
 import './index.css';
-import { Fragment } from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined} from '@ant-design/icons'
+const { Header, Footer, Sider, Content } = Layout;
+const {SubMenu } = Menu;
 
-moment.locale('zh-cn');
-
-const NewListItem=(item)=>{
-  return (
-  <div className="item-title" dangerouslySetInnerHTML={{__html:item}}>{item}</div>
-  )
-}
 function App() {
-  
-  const [isModalShow, setIsModalShow] = useState(false);
-  const [count, setCount] = useState(Initial);
-  const Counter = (Initial) =>{
-    const btnClick = () =>{
-      setIsModalShow(true)
-    }
-    return(
-      <Fragment>
-        <Button onClick={btnClick}>
-          Show Modal
-        </Button>
-        <Modal
-          visible={isModalShow}
-        >
-          
-        </Modal>
-      </Fragment>
-    )
-  }
+
   return (
-    <div className="App">
-      <Button onClick={() => {alert("hello")} }>确认</Button>
-    </div>
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item key="1" >nav 1</Menu.Item>
+          <Menu.Item key="2" >nav 2</Menu.Item>
+          <Menu.Item key="3" >nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{height:'100%', borderRight:0}}
+          >
+            <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+              <Menu.Item key="1">option1</Menu.Item>
+              <Menu.Item key="2">option2</Menu.Item>
+              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="4">option4</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<UserOutlined />} title="subnav 1">
+              <Menu.Item key="5">option5</Menu.Item>
+              <Menu.Item key="6">option6</Menu.Item>
+              <Menu.Item key="7">option7</Menu.Item>
+              <Menu.Item key="8">option8</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 1">
+              <Menu.Item key="9">option9</Menu.Item>
+              <Menu.Item key="10">option10</Menu.Item>
+              <Menu.Item key="11">option11</Menu.Item>
+              <Menu.Item key="12">option12</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+          Content
+        </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   );
-}
+};
 
 export default App;
