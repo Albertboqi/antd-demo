bdedf150120758e18a93ad712b3a20ed01910fce
albert wang <albertboqi@outlook.com>
2022-03-11 01:37:51 +0800

    更新我的代码

--- src/App.js
+++ src/App.js
@@ -1,62 +1,36 @@
-import React, { useState } from 'react';
-import { Button, ConfigProvider, Layout, Menu} from 'antd';
-// 由于 antd 组件的默认文案是英文，所以需要修改为中文
-import zhCN from 'antd/lib/locale/zh_CN';
-import moment from 'moment';
-import 'moment/locale/zh-cn';
-import 'antd/dist/antd.css';
-import './App.css';
-import AuthMange from './js/authManage'
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Button, Layout } from "antd";
+import "antd/dist/antd.css";
+import moment from "moment";
+import "moment/locale/zh-cn";
+import React, { useState } from "react";
+import "./App.css";
+moment.locale("zh-cn");
 
-const { Header, Sider, Content, Footer } = Layout;
-
-moment.locale('zh-cn');
+const { Content, Sider } = Layout;
 
 const App = () => {
-  
+  document.title = "首页";
+  const [showTree, setShowTree] = useState(true);
+
+  const onClick = () => {
+    if (showTree) {
+      setShowTree(false);
+    } else {
+      setShowTree(true);
+    }
+  };
   return (
-    <ConfigProvider locale={zhCN}>
-      <Layout>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onBreakpoint={broken => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
-          style={{height: window.innerHeight}}
-        >
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} >
-            <Menu.Item key="1" icon={<UserOutlined />}>
-              nav 1
-            </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-              nav 2
-            </Menu.Item>
-            <Menu.Item key="3" icon={<UploadOutlined />}>
-              nav 3
-            </Menu.Item>
-            <Menu.Item key="4" icon={<UserOutlined />}>
-              nav 4
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout>
-          <Header className="site-layout-sub-header-background" style={{ }} >
-            Security System
-          </Header>
-          <Content style={{ margin: '24px 16px 0' }}>
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 720 }}>
-              <AuthMange />
-            </div>
+    <Layout style={{ height: "100vh" }}>
+      <Sider theme="light">test</Sider>
+      <Content>
+        <Layout style={{ height: "100vh" }}>
+          {showTree ? <Sider></Sider> : ""}
+          <Content>
+            <Button onClick={onClick}>隐藏</Button>
           </Content>
         </Layout>
-      </Layout>
-    </ConfigProvider>
+      </Content>
+    </Layout>
   );
 };
 
